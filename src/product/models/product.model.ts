59ba994cb18c8b9c6d7353d356cee68378f40cb8
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2023-12-01 09:16:15 +0500

    smt

--- src/product/models/product.model.ts
+++ src/product/models/product.model.ts
@@ -39,7 +39,7 @@ export class Product extends Model<Product, ProductAttrs> {
   name: string;
 
   @Column({
-    type: DataType.FLOAT,
+    type: DataType.DECIMAL(20, 10),
   })
   price: number;
 
